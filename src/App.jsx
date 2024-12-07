865d1df207ef073565638b49d10d9caac8d686c0
emilyt <emilyz_2001@hotmail.com>
2024-12-07 21:13:41 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+// import './assets/all.scss'
 import './App.css'
 
 function App() {
@@ -16,7 +19,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React a</h1>
+      <h1>Vite + React b</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
